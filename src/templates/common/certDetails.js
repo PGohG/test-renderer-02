b60d35fe5ea846a4d2bed0c1f1031f1c5df90958
PGohG <gochbi@gmail.com>
2020-02-25 14:50:56 +0800

    Cert footnote

--- src/templates/common/certDetails.js
+++ src/templates/common/certDetails.js
@@ -199,7 +199,7 @@ export const renderITEFooter = doc => (
 			  <div className="col-1" />
 			  <div className="col-10 text-left">
 				<p style={arial5Pt}> Note: </p>
-				<div className="ml-8">
+				<div className="pl-8">
 					<p style={arial5Pt}>{doc.additionalData.footnoteLine1.slice(6)} {doc.additionalData.footnoteLine2}</p>
 				</div>
 			  </div>
